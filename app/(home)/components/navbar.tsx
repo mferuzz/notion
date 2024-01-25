ccd6d52506f3e73bc6d62f7c9c0c43859066a70c
feruzz <feruztelegraph@gmail.com>
2024-01-26 03:37:11 +0500

    Home Page UI

---
+++ app/(home)/components/navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { ModeToggle } from "@/components/shared/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { useScrolled } from "@/hooks/use-scrolled";
+import { cn } from "@/lib/utils";
+
+export const Navbar = () => {
+  const scrolled = useScrolled();
+  console.log(scrolled);
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center justify-between w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}>
+      <Logo />
+
+      <div className="flex items-center gap-x-2">
+        <Button size={"sm"} variant={"ghost"}>
+          Log In
+        </Button>
+        <Button size={"sm"}>Get Notion Free</Button>
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
